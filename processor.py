decc4e769e88ee5fd8b5a743080be7190407c52e
Dylan Sides <ds@ixis.co.uk>
2016-05-18 15:38:56 +0100

    Test

--- processor.py
+++ processor.py
@@ -2,6 +2,7 @@ import logging
 import json
 import requests
 import os
+from notify import Notify
 
 log = logging.getLogger("listener")
 
@@ -16,7 +17,8 @@ class Processor:
         self.access_key = os.getenv('CATTLE_ACCESS_KEY')
         self.secret_key = os.getenv('CATTLE_SECRET_KEY')
         self.domain = os.getenv('DOMAIN')
-        self.external_loadbalancer_port = os.getenv('LOADBALANCER_LISTEN_PORT','80')
+        self.external_loadbalancer_http_port = os.getenv('LOADBALANCER_HTTP_LISTEN_PORT', '80')
+        self.external_loadbalancer_https_port = os.getenv('LOADBALANCER_HTTPS_LISTEN_PORT', '443')
 
 
     def start(self):
@@ -31,7 +33,7 @@ class Processor:
         #for services, we only care if the status has become active, or removed
         if self.event['data']['resource']['state'] == 'active' or self.event['data']['resource']['state'] == 'removed':
             log.info('Detected a change in rancher services. Begin processing.')
-            log.debug(self._raw)
+            log.info(self._raw)
 
             #get the current event's stack information
             r = requests.get(self.event['data']['resource']['links']['environment'],
@@ -41,7 +43,12 @@ class Processor:
             r.raise_for_status()
             service_stack_response = r.json()
 
-
+            try:
+                notify = Notify(service_stack_response,
+                                'started' if self.event['data']['resource']['state'] == 'active' else 'stopped')
+                notify.send()
+            except:
+                log.error('An error occured while trying to notify stack change')
             # list of running stacks, called environments in api
             r = requests.get(self.api_endpoint + '/environments',
                              auth=(self.access_key, self.secret_key),
@@ -57,23 +64,29 @@ class Processor:
             for stack in stacks_response['data']:
                 stack_name = stack['name']
 
-                if stack_name == 'utility':
+                # make sure the stack/environment is active
+                if stack['state'] != 'active':
+                    log.info(' -- -- Ignoring {0} stack because it\'s not active'.format(stack_name))
+                    continue
+
+                if stack_name == 'load-balancer':
                     loadbalancer_service = self.get_utility_loadbalancer(stack)
-                else:
-                    depot_services = self.get_stack_services(stack)
 
-                    for service in depot_services:
 
-                        port = service['launchConfig'].get('labels',{}).get('depot.lb.port', '80')
-                        loadbalancer_entries.append({
-                            'serviceId': service['id'],
-                            'ports': [
-                                stack_name + '.' + self.domain + ':' + self.external_loadbalancer_port + '=' + port
-                            ]
-                        })
+                depot_services = self.get_stack_services(stack)
+
+                for service in depot_services:
+
+                    port = service['launchConfig'].get('labels',{}).get('depot.lb.port', '80')
+                    loadbalancer_entries.append({
+                        'serviceId': service['id'],
+                        'ports': [
+                            stack_name + '.' + self.domain + ':' + self.external_loadbalancer_http_port + '=' + port
+                        ]
+                    })
 
             if loadbalancer_service is None:
-                raise Exception('Could not find the Utility stack external load balancer. This should never happen')
+                raise Exception('Could not find the load-balancer stack external load balancer. This should never happen')
 
             log.info(' -- Setting loadbalancer entries:')
             log.info(loadbalancer_entries)
@@ -123,7 +136,7 @@ class Processor:
 
         #filter out any services that do not have the depot.lb.link label
         for service_data in services_response['data']:
-            if service_data['type'] != 'service': continue
+            if (service_data['type'] != 'service') and (service_data['type'] != 'externalService'): continue
             link = service_data['launchConfig'].get('labels',{}).get('depot.lb.link', 'false')
             if link == 'true':
                 depot_services.append(service_data)
