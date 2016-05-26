154bd4c8469e9c2a8099d1f2c95463b95b5dc7c1
Dylan Sides <demolicious@gmail.com>
2016-05-26 12:42:53 +0100

    revert test ports

--- processor.py
+++ processor.py
@@ -17,8 +17,8 @@ class Processor:
         self.access_key = os.getenv('CATTLE_ACCESS_KEY')
         self.secret_key = os.getenv('CATTLE_SECRET_KEY')
         self.domain = os.getenv('DOMAIN', 'zerofucks.co.uk')
-        self.external_loadbalancer_http_port = os.getenv('LOADBALANCER_HTTP_LISTEN_PORT', '81')
-        self.external_loadbalancer_https_port = os.getenv('LOADBALANCER_HTTPS_LISTEN_PORT', '444')
+        self.external_loadbalancer_http_port = os.getenv('LOADBALANCER_HTTP_LISTEN_PORT', '80')
+        self.external_loadbalancer_https_port = os.getenv('LOADBALANCER_HTTPS_LISTEN_PORT', '443')
 
 
     def start(self):
